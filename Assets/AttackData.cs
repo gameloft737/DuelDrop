6d11a9512b315038578b8d04aa78d5195cbfcd55
gameloft737 <91761862+gameloft737@users.noreply.github.com>
2024-09-28 23:10:28 -0400

    Added particle

--- Assets/AttackData.cs
+++ Assets/AttackData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AttackData", menuName = "Attacks/AttackData")]
 public class AttackData : ScriptableObject
 {
+    public GameObject particle;
     public float reloadSpeed = 1f;
     public float knockBack = 1f;    
     public float range = 1f;
--- Assets/WeaponManager.cs
+++ Assets/WeaponManager.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TreeEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class WeaponManager : MonoBehaviour
 {
@@ -11,22 +10,75 @@ public class WeaponManager : MonoBehaviour
     [SerializeField] AttackData specialAttack;
     [SerializeField] AttackData ultimateAttack;
 
-    [SerializeField]private PlayerMovement _playerMovement; // Reference to the PlayerMovement script
-    [SerializeField]private Transform target; // Reference to the target (opponent)
+
+    // References to the sliders in the UI
+    [SerializeField] private Slider regAttackSlider;
+    [SerializeField] private Slider specialAttackSlider;
+    [SerializeField] private Slider ultimateAttackSlider;
+
+    [SerializeField] private PlayerMovement _playerMovement; // Reference to the PlayerMovement script
+    [SerializeField] private Transform target; // Reference to the target (opponent)
+
+    private Dictionary<AttackData, float> attackCooldowns = new Dictionary<AttackData, float>();
+
+    private void Start()
+    {
+        // Initialize cooldowns for each attack
+        attackCooldowns[regAttack] = 0f;
+        attackCooldowns[specialAttack] = 0f;
+        attackCooldowns[ultimateAttack] = 0f;
+
+        regAttackSlider.value = regAttack.reloadSpeed;
+        specialAttackSlider.value = specialAttack.reloadSpeed;
+        ultimateAttackSlider.value = ultimateAttack.reloadSpeed;
     
+    }
+
+    private void Update()
+    {
+        // Update cooldown timers
+        List<AttackData> keys = new List<AttackData>(attackCooldowns.Keys);
+        foreach (AttackData attack in keys)
+        {
+            if (attackCooldowns[attack] > 0)
+            {
+                attackCooldowns[attack] -= Time.deltaTime;
+                UpdateSlider(attack);
+            }
+        }
+    }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            PerformAttack(regAttack);
+            TryPerformAttack(regAttack);
         }
     }
 
-   protected void PerformAttack(AttackData attack)
+    private void TryPerformAttack(AttackData attack)
+    {
+        if (attackCooldowns[attack] <= 0f)
+        {
+            PerformAttack(attack);
+            attackCooldowns[attack] = attack.reloadSpeed; // Set the cooldown based on reloadSpeed
+        }
+        else
+        {
+            Debug.Log("Attack on cooldown!");
+        }
+    }
+
+    protected void PerformAttack(AttackData attack)
     {
         if (target != null)
         {
+            // Instantiate the claw effect at the player's position
+            GameObject clawEffect = Instantiate(attack.particle, transform.position, Quaternion.identity, transform);
+
+            // Schedule destruction of the claw effect just before the attack reloads
+            StartCoroutine(DestroyClawEffect(clawEffect, attack.reloadSpeed));
+
             // Calculate the direction from the player to the target
             Vector3 directionToTarget = (target.position - transform.position).normalized;
 
@@ -50,8 +102,29 @@ public class WeaponManager : MonoBehaviour
         }
     }
 
+    private IEnumerator DestroyClawEffect(GameObject clawEffect, float delay)
+    {
+        // Wait for the delay (time until the next attack can be performed)
+        yield return new WaitForSeconds(delay);
+        Destroy(clawEffect);
+    }
 
 
 
 
+    private void UpdateSlider(AttackData attack)
+    {
+        if (attack == regAttack)
+        {
+            regAttackSlider.value = attackCooldowns[regAttack];
+        }
+        else if (attack == specialAttack)
+        {
+            specialAttackSlider.value = attackCooldowns[specialAttack];
+        }
+        else if (attack == ultimateAttack)
+        {
+            ultimateAttackSlider.value = attackCooldowns[ultimateAttack];
+        }
+    }
 }
