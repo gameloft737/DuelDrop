558bc4153ecc86dcfcd8097893fbea82454a1b5e
olivershih0 <130251690+olivershih0@users.noreply.github.com>
2024-11-02 17:49:41 -0400

    small changes

--- Assets/RandomEvent/Events/Bomb/BombEvent.cs
+++ Assets/RandomEvent/Events/Bomb/BombEvent.cs
@@ -7,7 +7,7 @@ public class BombEvent : RandomEvent
 {
     [SerializeField] private float spawnRadius;
     protected override Vector3 FindLocation(){
-        spawnRadius = EnvironmentManager.instance.GetBaseLength(3f);
+        spawnRadius = EnvironmentManager.instance.GetBaseLength(5f);
         Vector3 newPosition = new Vector3(UnityEngine.Random.Range(-spawnRadius, spawnRadius), 20f, 0);
         return newPosition;
     }
