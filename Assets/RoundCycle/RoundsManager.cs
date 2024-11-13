12a112e4c93099ef4951f7cd1243a8b14f07d2af
RishabhSudheer <sudheerrishabh@gmail.com>
2024-11-13 00:20:10 -0500

    RoundManager(Pt2)

--- Assets/RoundCycle/RoundsManager.cs
+++ Assets/RoundCycle/RoundsManager.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class RoundsManager : MonoBehaviour
 {
+    [SerializeField] public RoundCycle roundCycle;
+    [SerializeField] 
+    public int roundDuration;
+    public int roundNumber;
+    GameObject arrowKeyPlayer = GameObject.FindGameObjectWithTag("ArrowKeysPlayer");
+    GameObject WASDPlayer = GameObject.FindGameObjectWithTag("WASDPlayer");
+   
     public void RoundStart()
     {
+        roundDuration = roundCycle.roundDuration;
+        roundNumber = roundCycle.rounds;
+        for (int i = roundNumber; i > 0 ; i--)
+        {
+            
+            StartCoroutine(Clock(roundDuration));
+        }
 
     }
-    public RoundCycle roundCycle;
     private IEnumerator Clock(int roundtime)
     {
         while (roundtime > 0)
@@ -16,5 +31,9 @@ public class RoundsManager : MonoBehaviour
             roundtime--;
             yield return new WaitForSeconds(1);
         }
+    }
+    public void UpdateUI()
+    {
+            
     }
 }
