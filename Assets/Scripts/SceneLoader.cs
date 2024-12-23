9b55101e0ab05df27c16066d7b5ab445218905b1
RishabhSudheer <sudheerrishabh@gmail.com>
2024-12-23 14:02:44 -0500

    BackgroundIcons

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
@@ -12,8 +13,11 @@ public class SceneLoader : MonoBehaviour
     public int selectedWASD;
     public int selectedArrowKeys;
     public string[] sceneNames;
+    public Sprite[] sceneIcons;
+    public Image sceneImage;
     private void Start(){
         sceneIndex = 0;
+        sceneImage.sprite = sceneIcons[sceneIndex];
     }
     public void LoadScene()
     {
@@ -22,6 +26,8 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(sceneNames[sceneIndex]);
     }
     public void SetScene(int newPosition){
+        sceneImage.enabled = true;
         sceneIndex = newPosition;
+        sceneImage.sprite = sceneIcons[sceneIndex];
     }
 }
