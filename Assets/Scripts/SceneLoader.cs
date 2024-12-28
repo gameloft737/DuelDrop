a10af7796bf3bb7e5965f2c665e341b8e9b17bb1
olivershih0 <130251690+olivershih0@users.noreply.github.com>
2024-12-28 14:12:48 -0500

    win animation for all characters, new icons for map, map icon to background, small font change

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,38 +1,70 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 
 public class SceneLoader : MonoBehaviour
 {
     private int sceneIndex = 0;
     private int modeIndex = 1;
+
+
     // Selected player prefabs
-    public CharacterCycler WASDChararacters;
+    public CharacterCycler WASDCharacters;
     public CharacterCycler arrowKeysCharacters;
-    public int selectedWASD;
-    public int selectedArrowKeys;
     public string[] sceneNames;
-    public Sprite[] sceneIcons;
-    public Image sceneImage;
-    private void Start(){
-        sceneIndex = 0;
-        sceneImage.sprite = sceneIcons[sceneIndex];
+    public Texture2D[] sceneIcons;
+    public MeshRenderer sceneImage;
+
+
+    private void Start()
+    {
+        if (sceneIcons.Length > 0 && sceneImage != null)
+        {
+            sceneIndex = 0;
+            sceneImage.material.mainTexture = sceneIcons[sceneIndex];
+        }
+        else
+        {
+            Debug.LogError("Scene icons or scene image not set!");
+        }
     }
+
+
     public void LoadScene()
     {
-        PlayerPrefs.SetInt("selectedWASD",  WASDChararacters.currentIndex);
-        PlayerPrefs.SetInt("selectedArrowKeys",  arrowKeysCharacters.currentIndex);
-        
-        PlayerPrefs.SetInt("modeIndex",  modeIndex);
-        SceneManager.LoadScene(sceneNames[sceneIndex]);
+        if (sceneIndex < sceneNames.Length)
+        {
+            PlayerPrefs.SetInt("selectedWASD", WASDCharacters.currentIndex);
+            PlayerPrefs.SetInt("selectedArrowKeys", arrowKeysCharacters.currentIndex);
+            PlayerPrefs.SetInt("modeIndex", modeIndex);
+
+
+            SceneManager.LoadScene(sceneNames[sceneIndex]);
+        }
+        else
+        {
+            Debug.LogError("Scene index out of range!");
+        }
     }
-    public void SetScene(int newPosition){
-        sceneImage.enabled = true;
-        sceneIndex = newPosition;
-        sceneImage.sprite = sceneIcons[sceneIndex];
+
+
+    public void SetScene(int newPosition)
+    {
+        if (newPosition >= 0 && newPosition < sceneIcons.Length)
+        {
+            sceneImage.material.mainTexture = sceneIcons[newPosition];
+            sceneIndex = newPosition;
+        }
+        else
+        {
+            Debug.LogError("Invalid scene index!");
+        }
     }
-    public void SetMode(int newMode){
+
+
+    public void SetMode(int newMode)
+    {
         modeIndex = newMode;
     }
 }
